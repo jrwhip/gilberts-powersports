58df4006e3e6413f36483328bd26269a0993d1ae
Jared Whipple <Jared@Whipple.io>
2025-05-27 21:27:14 -0600

    Add meta data for Gilbert Power Sports

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,11 +6,11 @@ import '@/styles/tailwind.css'
 
 export const metadata: Metadata = {
   title: {
-    template: '%s - TaxPal',
-    default: 'TaxPal - Accounting made simple for small businesses',
+    template: '%s - Gilberts Power Sports',
+    default: 'Gilberts Power Sports - Your Segway Side by Side Headquarters',
   },
   description:
-    'Most bookkeeping software is accurate, but hard to use. We make the opposite trade-off, and hope you don’t get audited.',
+    'Discover the ultimate off-road adventure with our premium selection of Segway side by sides. Expert service, competitive prices, and unmatched performance await.',
 }
 
 const inter = Inter({
